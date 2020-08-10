10a81779ea3264b66de9d4cb4024eb4ca9fda735
fanqingyu <timo_quiet@163.com>
2020-08-10 21:43:09 +0800

    feat 请求模块单元测试requests

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -41,6 +41,7 @@ export default class Axios {
       config = url
     }
     config = mergeConfig(this.defaults, config)
+    config.method = config.method.toLowerCase()
     const chain: PromiseChain<any>[] = [
       {
         resolved: dispatchRequest,
